95f3ce272a3a25340b1ab27238ecc3cb8cf9f69e
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2022-09-13 22:43:51 -0500

    add row and col

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-
-    </div>
-  );
+		<div className="app">
+			<div className="container">
+				<div className="row">
+					<div className="col card">col</div>
+					<div className="col card">col</div>
+					<div className="col card">col</div>
+				</div>
+				<div className="row">
+					<div className="col card">col</div>
+					<div className="col card">col</div>
+					<div className="col card">col</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App;
